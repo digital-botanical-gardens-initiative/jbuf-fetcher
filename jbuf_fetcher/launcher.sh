1e089550abcfd32eda44dc07f8f3e1e992c2c7f8
edouardbruelhart <edouard.brulhart@gmail.com>
2025-03-25 09:54:17 +0100

    some cleaning

--- jbuf_fetcher/launcher.sh
+++ jbuf_fetcher/launcher.sh
@@ -61,7 +61,7 @@ echo "$MINUTE $HOUR $DAY"
 
 # Run fetcher_directus and taxo_resolver every 2 hours at 30
 if [[ "$MINUTE" == "30" && $((HOUR % 2)) -eq 0 ]]; then
-    run_script "fetcher_directus"
+    run_script "directus_fetcher"
     run_script "taxo_resolver"
 fi
 
