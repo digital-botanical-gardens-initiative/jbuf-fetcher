1aafebafea51311546b62ad0f651390bc8c578d6
HeloiseCoen <heloise.coen@gmail.com>
2025-03-31 17:43:47 +0200

    get the list

--- jbuf_fetcher/data_processing.py
+++ jbuf_fetcher/data_processing.py
@@ -4,27 +4,41 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Get the data lists 
-data_folder = os.getenv("DATA_PATH")
-file_path = os.path.join(data_folder, "resolved_data_directus.json")
+# Get the data lists
+data_folder = os.getenv("DATA_PATH") or ""
+file_path_directus = os.path.join(data_folder, "resolved_data_directus.json")
+file_path_botavista = os.path.join(data_folder, "resolved_data_botavista.json")
 
-if os.path.exists(file_path):
-    print("test")
-# Split the data lists by projects 
+def load_json(file_path, label):
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        print(f"{label} successfully charged")
+        return data
+    else:
+        print(f"Error : the file {file_path} does not exist")
+        return None
+    
+data_botavista = load_json(file_path_botavista, "Botavista")
+data_directus = load_json(file_path_directus, "Directus")
 
-# for each project : 
+print(f" Botavista Data : {data_botavista[:2] if data_botavista else 'not charged'}")
+print(f" Directus Data : {data_directus[:2] if data_directus else 'not charged'}")
+# Split the data lists by projects
 
-    # Compare botavista and directus 
+# for each project :
 
-    # Get the collected % 
+# Compare botavista and directus
 
-    # Compare botavista and directus "extracted = true"
+# Get the collected %
 
-    # Get the extracted % 
+# Compare botavista and directus "extracted = true"
 
-    # Compare botavista and directus "profiled = true"
+# Get the extracted %
 
-    # Get the profiled %
+# Compare botavista and directus "profiled = true"
+
+# Get the profiled %
 
 
-# Create a json with [project, % collected, % extracted, % profiled] (will be use in html_generator)
+# Create a json with [project, % collected, % extracted, % profiled] and json not_resolved_data (will be use in html_generator)
