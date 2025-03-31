6ae83b253daa5f407505ad72b367cfee8d99494b
edouardbruelhart <edouard.brulhart@gmail.com>
2025-03-31 16:15:28 +0200

    added extracted and profiled status

--- jbuf_fetcher/directus_fetcher.py
+++ jbuf_fetcher/directus_fetcher.py
@@ -1,10 +1,17 @@
 import json
 import os
+import re
 
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# Define session
+session = requests.Session()
+
+# Directus url
+base_url = "https://emi-collection.unifr.ch/directus"
 
 project_dict_str = os.getenv("PROJECT")
 
@@ -28,44 +35,48 @@ if not project_names:
     exit()
 
 
-# Define the Directus URLs
-field_name = "qfield_project"
-base_url = "https://emi-collection.unifr.ch/directus"
-collection_url = base_url + "/items/Field_Data"
-projects_filter = ",".join(project_names)
-request_url = (
-    collection_url
-    + f"?filter[{field_name}][_in]={projects_filter}&&limit=-1&fields=sample_id,sample_name,taxon_name,name_proposition,qfield_project"
-)
+def get_data() -> None:
+    # Make directus request
+    field_name = "qfield_project"
+    collection_url = base_url + "/items/Field_Data"
+    projects_filter = ",".join(project_names)
+    request_url = (
+        collection_url
+        + f"?filter[{field_name}][_in]={projects_filter}&&limit=-1&fields=sample_id,sample_name,taxon_name,name_proposition,qfield_project"
+    )
 
-# Define session
-session = requests.Session()
+    # Extract list from directus
+    params = {"sort[]": f"-{field_name}"}
+    response = session.get(request_url, params=params)
 
-# Extract list from directus
-params = {"sort[]": f"-{field_name}"}
-response = session.get(request_url, params=params)
+    if response.status_code == 200:
+        data_list = response.json().get("data", [])
+        parse_data(data_list)
 
-if response.status_code == 200:
-    # list_directus = response.json()["data"][0][field_name] if response.json()["data"] else "null"
-    data_list = response.json().get("data", [])
-    # Dictionary for unique sample name
-    unique_samples: dict[str, dict[str, str]] = {}
+    else:
+        print(f"error retreiving data: {response.status_code} - {response.text}")
+        exit()
 
+
+def parse_data(data_list: dict) -> None:
     # Merge taxon_name and sample_name
     filtered_data = []
+    # sample_id correct pattern
+    pattern = r"^[A-Za-z]{3,}_[0-9]{6}$"
+
     for entry in data_list:
         project = entry.get("qfield_project")
-        sample_id = entry.get("sample_id")
+        sample_id = entry.get("sample_id").replace(" ", "")
         sample_name = (entry.get("sample_name") or "").strip()
         taxon_name = (entry.get("taxon_name") or "").strip()
         name_proposition = (entry.get("name_proposition") or "").strip()
 
-        # Replace "aaunknown" (no name on the list)
-        if sample_name.lower() == "aaunknown":
-            if name_proposition:
-                sample_name = name_proposition
-            else:
-                continue  # ignore the line
+        # If sample_id doesn't match pattern, ignore sample
+        if not re.match(pattern, sample_id):
+            continue
+
+        # Check if sample is extracted and/or profiled
+        extracted, profiled = is_treated(sample_id)
 
         # Merge taxon_name with sample_name
         if not sample_name:
@@ -77,7 +88,13 @@ if response.status_code == 200:
 
         # Do not get the line if there is no sample_name
         if sample_name:
-            filtered_data.append({"species": sample_name, "sample_id": sample_id, "qfield_project": project})
+            filtered_data.append({
+                "species": sample_name,
+                "sample_id": sample_id,
+                "qfield_project": project,
+                "extracted": extracted,
+                "profiled": profiled,
+            })
 
     # Path file
     data_folder = os.getenv("DATA_PATH", ".")
@@ -90,5 +107,65 @@ if response.status_code == 200:
 
     print(f"Data have been succesfully saved in {data_file}")
 
-else:
-    print(f"error retreiving data: {response.status_code} - {response.text}")
+
+def is_treated(sample_id: str) -> tuple[bool, bool]:
+    container_id = get_container_id(sample_id)
+    if container_id == 0:
+        return False, False
+
+    # Check if the sample is extracted
+    extracted = get_child_container_id(container_id, "Extraction_Data", "sample_container")
+    if extracted == 0:
+        return False, False
+    
+    # Check if the sample is aliquoted
+    aliquoted = get_child_container_id(extracted, "Aliquoting_Data", "sample_container")
+    if aliquoted == 0:
+        return True, False
+    
+    # Check if the sample is profiled
+    profiled = get_child_container_id(aliquoted, "MS_Data", "id")
+    return True, profiled != 0
+    
+
+def get_child_container_id(container_id: int, collection: str, field_of_interest: str) -> int:
+    # Make directus request
+    field_name = "parent_sample_container"
+    collection_url = base_url + "/items/" + collection
+    request_url = collection_url + f"?filter[{field_name}][_eq]={container_id}&&limit=1&fields={field_of_interest}"
+
+    # Extract list from directus
+    response = session.get(request_url)
+
+    if response.status_code == 200:
+        if response.json()["data"] == []:
+            return 0
+        else:
+            child_container_id = response.json()["data"][0][field_of_interest]
+            return child_container_id
+    else:
+        print(f"Error: status: {response.status_code} - message: {response.text}")
+        exit()
+
+def get_container_id(sample_id: str) -> int:
+    # Make directus request
+    field_name = "container_id"
+    collection_url = base_url + "/items/Containers"
+    request_url = collection_url + f"?filter[{field_name}][_eq]={sample_id}&&limit=1&fields=id"
+
+    # Extract list from directus
+    response = session.get(request_url)
+
+    if response.status_code == 200:
+        if response.json()["data"] != []:
+            container_id = int(response.json()["data"][0]["id"])
+            return container_id
+        else:
+            return 0
+
+    else:
+        print(f"Error: status: {response.status_code} - message: {response.text}")
+        exit()
+
+
+get_data()
