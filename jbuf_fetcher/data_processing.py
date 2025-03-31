bcc1a6872477f117a966aced4ee7e570503fb148
edouardbruelhart <edouard.brulhart@gmail.com>
2025-03-31 16:52:23 +0200

    add data_processing.py

---
+++ jbuf_fetcher/data_processing.py
@@ -0,0 +1,19 @@
+# Get the data lists 
+
+# Split the data lists by projects 
+
+# for each project : 
+
+    # Compare botavista and directus 
+
+    # Get the collected % 
+
+    # Compare botavista and directus "extracted = true"
+
+    # Get the extracted % 
+
+    # Compare botavista and directus "profiled = true"
+
+    # Get the profiled %
+
+# Create a json with [project, % collected, % extracted, % profiled] (will be use in html_generator)
